be52502d7aa75c47ba942a27f54c737cff1c37d7
Fred Strauss <fred@strider.co.za>
2020-02-18 21:41:22 +0200

    Initial table showing

--- src/AnsiTable.hs
+++ src/AnsiTable.hs
@@ -1,6 +1,69 @@
 module AnsiTable
-    ( someFunc
+    ( Cell (..)
+    , Alignment (..)
+    , cellFromString
+    , showStringTable
+    , showTable
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List
+import System.Console.ANSI
+
+type Width = Int
+data Alignment = AlignLeft | AlignRight
+
+data Cell = Cell { sgr :: [SGR]
+                 , content :: String
+                 , align :: Alignment
+                 }
+
+cellFromString :: String -> Cell
+cellFromString cs = Cell { sgr = [Reset]
+                         , content = cs
+                         , align = AlignLeft
+                         }
+
+maxLength :: [Cell] -> Width
+maxLength = foldl (\acc x -> if length (content x) > acc then length (content x) else acc) 0
+
+zipWidths :: [Width] -> [Cell] -> [(Cell, Width)]
+zipWidths ws cs = zip cs ws
+
+putPadded :: (Cell, Width) -> IO ()
+putPadded (c,w) = do
+  setSGR $ sgr c
+  putStr $ s
+  putStr $ replicate (w+1-length s) ' '
+  setSGR [ Reset ]
+  where s = content c
+
+putPaddedLine :: [(Cell, Width)] -> IO ()
+putPaddedLine cs = do
+  mapM_ putPadded cs
+  putStrLn ""
+
+showTable :: [[Cell]] -> IO ()
+showTable t = do
+  mapM_ putPaddedLine z
+  where tt = transpose t
+        widths = map maxLength tt
+        z = map (zipWidths widths) t
+
+showStringTable :: [[String]] -> IO ()
+showStringTable t = do
+  mapM_ putPaddedLine z
+  where tt = transpose ct
+        widths = map maxLength tt
+        z = map (zipWidths widths) ct
+        ct = map (map cellFromString) t
+
+{--
+ - (H1, C1W), (H2, C2W)
+ - (V1, C1W), (V2, C2W)
+ --}
+ 
+{--
+a = [ ["H1", "H2", "H3"]
+    , ["V1", "V2", "V3"]
+    ]
+--}
